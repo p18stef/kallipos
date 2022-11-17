d7bd41f3f3094fd42caec3e04073f16a5d1689e5
p18stef <115227021+p18stef@users.noreply.github.com>
2022-11-17 20:23:17 +0200

    Create arduino.lua

---
+++ arduino.lua
@@ -0,0 +1,13 @@
+function Image(img)
+  if img.classes:find('arduino',1) then
+    local f = io.open("arduino/" .. img.src, 'r')
+    local doc = pandoc.read(f:read('*a'))
+    f:close()
+    local caption = pandoc.utils.stringify(doc.meta.caption) or "Epigraph has not been set"
+    local student = pandoc.utils.stringify(doc.meta.student) or "Student has not been set"
+    local studentid = pandoc.utils.stringify(doc.meta.studentid) or "Student ID has not been set"
+    local credentials = "Student: " .. student .. " (" .. studentid .. ")"
+    local comment = "> _" .. caption .. "  \n> " .. "*" .. credentials .. "*"
+    return pandoc.RawInline('markdown',comment)
+  end
+end
