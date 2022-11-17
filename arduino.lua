234cfad0f734d3de98337fd20f3e2f9e9ca891fd
p18stef <115227021+p18stef@users.noreply.github.com>
2022-11-17 20:32:52 +0200

    Update arduino.lua

--- arduino.lua
+++ arduino.lua
@@ -7,7 +7,7 @@ function Image(img)
     local student = pandoc.utils.stringify(doc.meta.student) or "Student has not been set"
     local studentid = pandoc.utils.stringify(doc.meta.studentid) or "Student ID has not been set"
     local credentials = "Student: " .. student .. " (" .. studentid .. ")"
-    local comment = "> _" .. caption .. "  \n> " .. "*" .. credentials .. "*"
+    local comment = "> _" .. caption .. "_ \n> " .. "**" .. credentials .. "**"
     return pandoc.RawInline('markdown',comment)
   end
 end
