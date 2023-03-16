ed43f875abdaabb1304c3953ca3dc0daf892941c
p18stef <115227021+p18stef@users.noreply.github.com>
2023-03-16 09:56:39 -0500

    Add files via upload

--- make-latex.sh
+++ make-latex.sh
@@ -6,7 +6,7 @@ pandoc text/intro.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-lev
 
 for filename in text/ch*.txt; do
    [ -e "$filename" ] || continue
-   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=arduino.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=footnote.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to latex > latex/"$(basename "$filename" .txt).tex"
+   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=arduinomemory.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=footnote.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to latex > latex/"$(basename "$filename" .txt).tex"
 done
 
 pandoc text/epi.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/epi.tex
@@ -19,7 +19,7 @@ done
 
 #sed -i '' 's+Figure+Εικόνα+g' ./latex/ch0*
 pandoc -s latex/*.tex -o book.tex
-pandoc -N --quiet --variable "geometry=margin=1.2in" --variable mainfont="Nimbus Sans" --variable sansfont="Nimbus Sans" --variable monofont="Nimbus Sans" --variable fontsize=12pt --variable version=2.0 book.tex --pdf-engine=xelatex --toc -o book.pdf
+pandoc -N --quiet --variable "geometry=margin=1.2in" --variable mainfont="MesloLGS NF" --variable sansfont="MesloLGS NF" --variable monofont="MesloLGS NF" --variable fontsize=12pt --variable version=2.0 book.tex --pdf-engine=xelatex --toc -o book.pdf
 
 pandoc -o book.epub book.tex
 mv book.epub book/book.epub
